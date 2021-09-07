783ced0cd2ce2c716f11bfd258bd49d2999764c0
jineshnagori <jineshn21@gmail.com>
2021-09-07 11:34:02 +0530

    changes in admin panel

--- admin/admin.php
+++ admin/admin.php
@@ -26,7 +26,6 @@
         <a id="button" class="btn text-white" href="logout.php">Logout</a>
     </div>
 
-    <!-- Services -->
     <section class="mt-4">
         <div class="container"  style="overflow-x:auto;">
             <!-- table -->
@@ -64,6 +63,96 @@
                 </tbody>
             </table>
         </div>
+    </section>
+
+    <section class="mt-4">
+        <div class="container">
+            <div class="row text-center">
+                <h2 class="fw-bold mb-3">App Projects</h2>
+                <div class="heading-line mb-5"></div>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-lg-6 col-md-12 bg-white p-3">
+          <div class="form w-100 pb-2">
+            <form method="POST" action="app.php" class="row" enctype="multipart/form-data">
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <input name="pname" type="text" placeholder="Project Name" class="form-control form-control-lg">
+              </div>
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <textarea name="pdesc" placeholder="Project Description" id="message" rows="8" class="form-control form-control-lg"></textarea>
+              </div>
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <input type="file" name="images[]" multiple class="form-control form-control-lg">
+              </div>
+              <div class="col-lg-12 col-md-12 mb-3 d-grid text-center">
+                <button type="submit" name="submit" id="button" class="rounded-pill pt-3 pb-3">SUBMIT</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="mt-4">
+        <div class="container">
+            <div class="row text-center">
+                <h2 class="fw-bold mb-3">Web Projects</h2>
+                <div class="heading-line mb-5"></div>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-lg-6 col-md-12 bg-white p-3">
+          <div class="form w-100 pb-2">
+            <form method="POST" action="web.php" class="row" enctype="multipart/form-data">
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <input name="pname" type="text" placeholder="Project Name" class="form-control form-control-lg">
+              </div>
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <textarea name="pdesc" placeholder="Project Description" id="message" rows="8" class="form-control form-control-lg"></textarea>
+              </div>
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <input type="file" name="fileUpload[]" multiple class="form-control form-control-lg">
+              </div>
+              <div class="col-lg-12 col-md-12 mb-3 d-grid text-center">
+                <button type="submit" name="submit" id="button" class="rounded-pill pt-3 pb-3">SUBMIT</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="mt-4">
+        <div class="container">
+            <div class="row text-center">
+                <h2 class="fw-bold mb-3">Python Projects</h2>
+                <div class="heading-line mb-5"></div>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-lg-6 col-md-12 bg-white p-3">
+          <div class="form w-100 pb-2">
+            <form method="POST" action="python.php" class="row" enctype="multipart/form-data">
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <input name="pname" type="text" placeholder="Project Name" class="form-control form-control-lg">
+              </div>
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <textarea name="pdesc" placeholder="Project Description" id="message" rows="8" class="form-control form-control-lg"></textarea>
+              </div>
+              <div class="col-12 mb-3" data-aos="fade-down" data-aos-duration="8000">
+                <input type="file" name="fileUpload[]" multiple class="form-control form-control-lg">
+              </div>
+              <div class="col-lg-12 col-md-12 mb-3 d-grid text-center">
+                <button type="submit" name="submit" id="button" class="rounded-pill pt-3 pb-3">SUBMIT</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
 
     <?php
